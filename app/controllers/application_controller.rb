24afbe086c7c5b47717b9753f6ee7d61354de236
snakayama <hancldrsn@gmail.com>
2016-07-23 13:32:45 +0900

    error process changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # 他のエラーハンドリングでキャッチできなかった場合に
+  # 500 Internal Server Error(システムエラー)を発生させる
+  # NOTE: rescue_from は下から評価されるので記載箇所はここが正解
+  rescue_from Exception, with: :handle_500 unless Rails.env.development?
   
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
@@ -19,5 +24,17 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
       devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
+    end
+
+      # エラーハンドリング処理
+    def handle_500(exception = nil)
+      logger.info "Rendering 500 with exception: #{exception.message}" if exception
+
+      if request.xhr?
+        # Ajaxのための処理
+        render json: { error: '500 error' }, status: 500
+      else
+        render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+      end
     end
 end
