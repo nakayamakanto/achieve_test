a6c7d00422c57f4996492d3f39fbd574c19915c2
snakayama <hancldrsn@gmail.com>
2016-07-23 15:12:13 +0900

    remeve unique of email

---
+++ db/migrate/20160723055500_remove_user_email_unique.rb
@@ -0,0 +1,5 @@
+class RemoveUserEmailUnique < ActiveRecord::Migration
+  def change
+    remove_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720105529) do
+ActiveRecord::Schema.define(version: 20160723055500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 20160720105529) do
     t.string   "avatar"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
