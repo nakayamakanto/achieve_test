93fc8b26a13ec59991c3a38e4f4ee4234d9b560b
snakayama <hancldrsn@gmail.com>
2016-07-23 13:52:25 +0900

    routes.rb changed

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     #下記の２つがあると、ログアウトがうまくいかなくなる。おそらく、実際にはない/users/sessions_controllerやusers/passwords_controllerを探しに行ってしまうから
     # sessions: "users/sessions",
     # password: "users/passwords",
-}
+  }
   
   resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy, :show] do
     collection do
@@ -22,4 +22,6 @@ Rails.application.routes.draw do
   end
   
   root "top#index"
+
+  match "*path" => "application#handle_404", via: :all
 end
